7cba5b3d0be807914487fcd3919bc19c10641f1d
AMARDEEP7263 <amardeeptomar7263@gmail.com>
2024-06-19 21:51:25 +0530

    css changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import "./Home.css"; // Import the CSS file
 
 const Home = () => {
   return (
     <div className="home-container">
+      <div className="stars"></div> {/* Add the stars element */}
       <div className="home-content">
         <h1>Welcome to Our Landing Page</h1>
+        <p>This is the home page of our awesome site.</p>
       </div>
     </div>
   );
